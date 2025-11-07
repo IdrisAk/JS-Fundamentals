50f69f3156037b1303e58cc00deb3f0d721d3029
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-07 17:07:19 +0100

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,8 @@
 const { argv } = require('node:process');
 const args = process.argv.slice(2);
-console.log(`"${args[0]} ${args[2]}");
-if (!isNaN(Number(args)){
-  console.log(`My number: ${args[0]}`)
+
+if (!isNaN(Number(args[0])){
+  console.log(`My number: ${args[0]}`);
   }
 else{
-  console.log("Not a number")
+  console.log("Not a number");
