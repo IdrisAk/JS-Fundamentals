702d883074181f69ac6a66960282e9b077c4a35a
Idris Ak <idrisakdahiru@gmail.com>
2025-11-07 11:55:51 +0100

    corrections in the code

---
+++ 2-value_argument.js
@@ -0,0 +1,14 @@
+
+function argument ( ){
+    if (arguments[0] == undefined){
+        console.log("No argument")
+    }
+    else if(arguments[0] =! undefined){
+        console.log("Argument found")
+    }
+    else
+    {
+        console.log("Arguments found")
+    }
+}
+ argument(2);
