36d9c6b84a55dac464801bb16f64ef967c0b92f4
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-07 16:08:29 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,6 +2,9 @@ const { argv } = require('node:process');
 const args = process.argv.slice(2);
 args.forEach(function(item) {
   console.log(item);
+  if (item === undefined){
+    console.log("No argument")
+  }
 });
 
 if (args[0] === undefined) {
