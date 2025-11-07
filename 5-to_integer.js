b11b873d85d54c666885334d24a66c24c4b12151
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-07 17:04:26 +0100

    Create 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+const { argv } = require('node:process');
+const args = process.argv.slice(2);
+console.log(`"${args[0]} ${args[2]}");
+if (!isNaN(Number(args)){
+  console.log(`My number: ${args[0]}`)
+  }
+else{
+  console.log("Not a number")
