81543dd86ca553923dbcc057f716002cc89f8847
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-09 08:39:05 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -4,9 +4,10 @@ const args = process.argv.slice(2);
 if (!isNaN(Number(args[0]))){
   for (let i = 1; i <= args[0]; i++){
     console.log("x");
+    let x = " ";
     for (let i = 1; i <= args[0]; i++){
-      let x = " ";
-      x += i
+    
+      x += "x";
       console.log(x);
   }
   }}
