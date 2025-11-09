0c7dcb1c609b5cc239322a380b20e4fb6a67e152
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-09 08:58:54 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -2,15 +2,15 @@ const { argv } = require('node:process');
 const args = process.argv.slice(2);
 
 if (!isNaN(Number(args[0]))){
- /* for (let i = 1; i <= args[0]; i++){
-    console.log("x"); */
+ for (let i = 1; i <= args[0]; i++){
+    
     let x = " ";
     for (let i = 1; i <= args[0]; i++){
     
       x += "x";
        }
      console.log(x);
-  }
+  }}
 else{
   console.log("Missing size");
 }
