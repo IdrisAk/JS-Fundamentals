cc0c836d17bf0644104033e8eac0b9e85cdb2d16
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-09 08:26:17 +0100

    Create 8-square.js
    it prints square i used nested loop

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+const { argv } = require('node:process');
+const args = process.argv.slice(2);
+
+if (!isNaN(Number(args[0]))){
+  for (let i = 1; i <= args[0]; i++){
+    console.log("x");
+    for (let i = 1; i <= args[0]; i++){
+    console.log("x");
+  }
+  }}
+else{
+  console.log("Missing size");
+}
