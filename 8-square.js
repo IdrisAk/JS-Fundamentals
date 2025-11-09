93d77c3a6b00bfeda503bbfe2cf3c506e6c55a5a
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-09 08:51:41 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -2,8 +2,8 @@ const { argv } = require('node:process');
 const args = process.argv.slice(2);
 
 if (!isNaN(Number(args[0]))){
-  for (let i = 1; i <= args[0]; i++){
-    console.log("x");
+ /* for (let i = 1; i <= args[0]; i++){
+    console.log("x"); */
     let x = " ";
     for (let i = 1; i <= args[0]; i++){
     
