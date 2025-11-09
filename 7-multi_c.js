8ade18180582c897a4d52bb298c0738c149a8970
IdrisAk <34672260+IdrisAk@users.noreply.github.com>
2025-11-09 08:00:24 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 if (!isNaN(Number(args[0]))){
   for (let occurences = 0; occurences >= args[0]; occurences ++){
   console.log("C is fun");
-  }
+  }}
 else{
   console.log("Missing number of occurrences”);
 }
